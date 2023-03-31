bbf8a779f8c1f234184e94d58f1cf6a0a94b42e9
player01osu <player01osu@gmail.com>
2023-03-31 19:23:11 -0400

    Move button onevents.

--- js/shell/move.js
+++ js/shell/move.js
@@ -3,5 +3,78 @@ const move_front = document.getElementById("move_front");
 const move_back = document.getElementById("move_back");
 const move_checked = document.getElementById("move_checked");
 
-// Need image path, and audio path
+move_all.onclick = () => {
+    let from_files = "";
+    for (let i = cache.head; i < cache.tail; ++i) {
+        const element = cache.elements[i];
+        if (exists(element.audio_path)) {
+            from_files += "'" + element.audio_path + "' ";
+        }
+        if (exists(element.image_path)) {
+            from_files += "'" + element.image_path + "' ";
+        }
+    }
 
+    if (exists(from_files)) {
+        code_set("cp", from_files + " './collection.media/'");
+    }
+}
+
+move_front.onclick = () => {
+    let from_files = "";
+    const element = cache.elements[cache.head];
+    if (exists(element.audio_path)) {
+        from_files += "'" + element.audio_path + "' ";
+    }
+    if (exists(element.image_path)) {
+        from_files += "'" + element.image_path + "' ";
+    }
+
+    if (exists(from_files)) {
+        code_set("cp", from_files + " './collection.media/'");
+    }
+}
+
+move_back.onclick = () => {
+    let from_files = "";
+    const element = cache.elements[cache.tail - 1];
+    if (exists(element.audio_path)) {
+        from_files += "'" + element.audio_path + "' ";
+    }
+    if (exists(element.image_path)) {
+        from_files += "'" + element.image_path + "' ";
+    }
+
+    if (exists(from_files)) {
+        code_set("cp", from_files + " './collection.media/'");
+    }
+}
+
+move_checked.onclick = () => {
+    const checked = get_checked();
+    const text_set = {};
+    if (checked.length === 0) {
+        return;
+    }
+
+    for (let i = 0; i < checked.length; ++i) {
+        text_set[checked[i].firstChild.textContent] = true;
+    }
+
+    let from_files = "";
+    for (let i = cache.head; i < cache.tail; ++i) {
+        const element = cache.elements[i];
+        if (exists(text_set[element.text])) {
+            if (exists(element.audio_path)) {
+                from_files += "'" + element.audio_path + "' ";
+            }
+            if (exists(element.image_path)) {
+                from_files += "'" + element.image_path + "' ";
+            }
+        }
+    }
+
+    if (exists(from_files)) {
+        code_set("cp", from_files + " './collection.media/'");
+    }
+}
